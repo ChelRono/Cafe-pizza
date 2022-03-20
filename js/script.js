d36c87570c2af13f1719e52de90bfef1ac59fd0b
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-03-20 18:38:37 -0400

    added data in js

---
+++ js/script.js
@@ -0,0 +1,69 @@
+var data = {
+    pizza :[
+        {
+            id: 1,
+            name: 'Small',
+            price: 750
+        },
+        {
+            id: 2,
+            name: 'Medium',
+            price: 1000
+        },
+        {
+            id: 3,
+            name: 'Large',
+            price: 1250
+        }
+    ],
+         toppings:[
+            {
+                id: 1,
+                name: 'Olive',
+                price: 100
+            },
+            {
+                id: 2,
+                name: 'Cheese',
+                price: 120
+            },
+            {
+                id: 3,
+                name: 'Pepperoni',
+                price: 90
+            },
+            {
+                id: 4,
+                name: 'Mushroom',
+                price: 100
+            },
+            {
+                id: 5,
+                name:'Sausage',
+                price: '90',
+            },
+            {
+                id: 6,
+                name:'Pineapple',
+                price: '80'
+            }
+
+        ],
+        crust:[
+            {
+                id: 1,
+                name: 'flatbread',
+                price: 100
+            },
+            {
+                id: 2,
+                name: 'thin crust',
+                price: 120
+            },
+            {
+                id: 3,
+                name: 'stuffed',
+                price: 90
+            },
+        ]
+    }
