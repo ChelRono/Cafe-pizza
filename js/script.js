040415011c163dbe246d4e992ef0f505b5577da7
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-03-21 17:34:01 -0400

    added hide/show options

--- js/script.js
+++ js/script.js
@@ -7,7 +7,29 @@
 //     </tr>)
     
 //     });
-    
+$(".table").hide();
+$(".info").hide();
+$("#yes").hide();
+$("#no").hide();
+$(".info h4").hide();
+
+$('.btn btn-primary').click(function() {
+  var sizeOfPizza = $("#size").val();
+  var toppingsOfPizza = $("#toppings").val();
+  var crustOfPizza = $("#crust").val();
+  var total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
+  var order = 1;
+  var grandTotal = 0;
+
+  $("table").show();
+  $(".additional-buttons").show();
+  $(".btn.order").hide();
+
+  $("#size").html($(".size option:selected").text() + " - " + sizeOfPizza);
+  $("#toppings").html($(".toppings option:selected").text() + " - " + toppingsOfPizza);
+  $("#crust").html($(".crust option:selected").text() + " - " + crustOfPizza);
+  $("#total").html(total);
+
 function Pizza(OrderNo, Size, Toppings, Crust, Total) {
     this.OrderNo = OrderNo;
     this.Size = Size;
