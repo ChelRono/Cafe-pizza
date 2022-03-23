5e945e5179337ec9f312646a0dafd92f3a4a7cf9
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-03-23 12:43:56 -0400

    made some changes

--- js/script.js
+++ js/script.js
@@ -1,61 +1,75 @@
 
-$(".table").hide();
-$("#add-order").hide();
+/*$(".table").hide();
+$(".additional-info").hide();
 $(".info").hide();
 $("#yes").hide();
 $("#no").hide();
-$(".received").hide();
+$(".received").hide();*/
 
 
 $(document).ready(function() {
-  $("#order").click(function(event) {
-      event.preventDefault();
+  $('#order1').click(function(event) {
 
-  var sizeOfPizza = $(".size option:selected").val();
-  var toppingsOfPizza = $(".toppings option:selected").val();
-  var crustOfPizza = $(".crust option:selected").val();
-  var total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
-  var order = 1;
-  var grandTotal = 0;
 
-  $(".table").show();
-  $("#add-order").show();
-  $("#order").hide();
+    event.preventDefault();
 
-  $("#Size").html($(".size option:selected").text() + " - " + sizeOfPizza);
-  $("#Toppings").html($(".toppings option:selected").text() + " - " + toppingsOfPizza);
-  $("#Crust").html($(".crust option:selected").text() + " - " + crustOfPizza);
-  $("#Total").html(total);
+    var sizeOfPizza = parseInt($(".size option:selected").val());
+    var toppingsOfPizza = parseInt($(".toppings option:selected").val());
+    var crustOfPizza = parseInt($(".crust option:selected").val());
+    var numberOfPizza = parseInt($("#number-pizza").val());
 
-  function Pizza(Size, Toppings, Crust, total, OrderNo,) {
-      this.Size = Size;
-      this.Toppings = Toppings;
-      this.Crust = Crust;
-      this.Total = total;
-      this.OrderNo = OrderNo;
-  }
-  $('add-order').click(function() {
+
+    var total = (sizeOfPizza + toppingsOfPizza + crustOfPizza)*numberOfPizza;
+    var order = 1;
+    var grandTotal = 0;
+
+    $(".table").show();
+    $("#add-order").show();
+    $("#order1").hide();
+
+  
+
+    $(".size option:selected").text();
+  
+    $(".toppings option:selected").text();
+  
+    $(".crust option:selected").text();
+  
+    $("#total").html(total);
+    
+
+
+    function  Pizza(size, toppings, crust, total, orderNo) {
+      this.size = size;
+      this.toppings = toppings;
+      this.crust = crust;
+      this.total = total;
+      this.orderNo = orderNo;
+    }
+  $('#add-order').click(function(event) {
+    event.preventDefault();
     var sizeOfPizza = $(".size option:selected").val();
     var toppingsOfPizza = $(".toppings option:selected").val();
     var crustOfPizza = $(".crust option:selected").val();
     var total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
     order = order + 1;
     grandTotal = grandTotal + total;
-
+    
 
     var newPizza = new Pizza(sizeOfPizza, toppingsOfPizza, crustOfPizza, grandTotal, order );
 
     
+    // var newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="size">' + $(".size option:selected").text() + " - " + newPizza.size + '</td><td id="toppings">' + $(".toppings option:selected").text() + " - " + newPizza.toppings + '</td><td id="crust">' + $(".crust option:selected").text() + " - " + newPizza.crust + '</td><td id="total">' + newPizza.total + '</td></tr>'
+    var newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="size">' + $(".size option:selected").text()+ '</td><td id="toppings">' + $(".toppings option:selected").text()+ '</td><td id="crust">' + $(".crust option:selected").text()+ '</td><td id="total">' + newPizza.total + '</td></tr>'
 
-    var newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="size">' + $(".size option:selected").text() + " - " + newPizza.size + '</td><td id="toppings">' + $(".toppings option:selected").text() + " - " + newPizza.toppings + '</td><td id="crust">' + $(".crust option:selected").text() + " - " + newPizza.crust + '</td><td id="total">' + newPizza.total + '</td></tr>'
-
+    
     $("#pizza").append(newRow);
-   
   });
 
   $("#checkout").click(function() {
     $("#add-order").hide();
     $("#checkout").hide();
+    $(".additional-info").show();
     $(".info").show();
     $("#yes").show();
     $("#no").show();
@@ -70,10 +84,10 @@ $(document).ready(function() {
     (grandTotal + 150);
   });
   $("#no").click(function() {
-    $("delivery").hide();
+    $(".delivery").hide();
     $("#yes").hide();
     $("#no").hide();
-    // $(".location").show();
+    $(".location").show();
     alert("Your order is ready!")
   });
   $("#done").click(function() {
@@ -83,7 +97,7 @@ $(document).ready(function() {
     $(".received ").html(location);
     alert("Your order will be dispatched to you shortly!")
   });
-  
 });
+
 })
 
